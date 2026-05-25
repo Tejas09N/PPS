6aa3f8903a23f2c44c24aa631885a5cf03d885d1
Tejas09N <sakshambhoyar03@gmail.com>
2026-05-25 18:19:29 +0530

    Add files via upload

---
+++ Swap.c
@@ -0,0 +1,18 @@
+// Name- Tejas S. Bhoyar
+// PRN no-2503033111372025
+#include <stdio.h>
+int main() {
+    int a,b;
+      printf("Enter Number A: ");
+      scanf("%d",&a);
+      printf("Enter Number B: ");
+      scanf("%d",&b);
+
+    a=a+b;
+    b=a-b;
+    a=a-b;
+
+
+    printf("a = %d, b = %d\n", a, b);
+    return 0;
+}
