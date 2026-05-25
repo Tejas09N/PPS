125c49b4f5a51fc254854a0a1538ce865b28361e
Tejas09N <sakshambhoyar03@gmail.com>
2026-05-25 18:22:05 +0530

    Add files via upload

---
+++ star2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // Loop for 5 rows
+    for(int i = 0; i < 5; i++) {
+        // Print 5 stars in each row
+        for(int j = 0; j < 5; j++) {
+            printf("*");
+        }
+        // Move to the next line after each row
+        printf("\n");
+    }
+    return 0;
+}
