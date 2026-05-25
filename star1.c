dc889d6e55fe8af149c3ae44827589cbd2947abd
Tejas09N <sakshambhoyar03@gmail.com>
2026-05-25 18:18:54 +0530

    Add files via upload

---
+++ star1.c
@@ -0,0 +1,16 @@
+// Name- Tejas S. Bhoyar
+// PRN no-2503033111372025
+#include <stdio.h>
+int main(){
+    int i,j,k;
+    for(i=0;i<5;i++){
+        for(k=0 ;k<5-i;k++){
+
+        }
+
+        for(j=0 ;j<5-i;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+}
